b081b56fb82beffb4c917d9be2c7c1a595832127
Tate <tatebraeckel@gmail.com>
2022-09-19 20:58:21 -0600

    commit tree 97b47925b0043e4c6708ded0c85d4b91528f62c3

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const input = document.getElementById('button');
+
 function addingEventListener() {
+    input.addEventListener('click', function () {
+        alert('I was clicked!');
+    });
 }
+
+
+
